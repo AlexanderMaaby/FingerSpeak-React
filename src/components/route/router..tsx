01724b970ef18516310a151b93e89067d627b9c5
maaby200 <maaby200@gmail.com>
2021-09-21 13:20:45 +0200

    feat: setting up router

---
+++ src/components/route/router..tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+const Router = () => {
+    return (
+<BrowserRouter>
+    <Switch>
+        <Route path="" component={} exact={}/>
+        <Route path="" component={} exact={}/>
+        <Route path="" component={} exact={}/>
+    </Switch>
+</BrowserRouter>
+    )
+}
+export default Router
