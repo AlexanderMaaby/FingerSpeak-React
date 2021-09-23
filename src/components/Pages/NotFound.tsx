3487920cc73d5c71c97f171359c0e9baf87c8b7c
Krister Emanuelsen <SeventhDisaster@outlook.com>
2021-09-23 23:04:54 +0200

    feat: styled 404 page

--- src/components/Pages/NotFound.tsx
+++ src/components/Pages/NotFound.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router'
+import styles from './NotFound.module.css'
 
 const NotFound = () => {
 
+    const history = useHistory();
+
     document.title = "404 - Not Found"
 
+    const onTakeBack = () => {
+        history.replace("/");
+    }
+
     return (
-        <div>
+        <div className={styles.NotFoundBody} >
             <h1>404 Not Found</h1>
+            <p> Whoops! Looks like you got lost! </p>
+            <button className={styles.TakeBackButton} onClick={onTakeBack}> Take Me Back </button>
         </div>
     )
 }
